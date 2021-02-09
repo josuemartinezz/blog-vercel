c2d0a579cd8e1608eb254139b0b156b342154180
josue.ayala <ayalavalencia11@gmail.com>
2021-02-09 08:41:15 -0600

    🎨 Poppins typeface installed

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+      }
+    ],
     script: []
   },
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'ui-sans-serif', 'system-ui', 'sans-serif']
+    },
     extend: {}
   },
   variants: {
