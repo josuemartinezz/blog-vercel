d5d5f4592fe4ef85ac43181229de23eb67ef7ddf
josue.ayala <ayalavalencia11@gmail.com>
2021-02-09 08:25:46 -0600

    👗 The whole project was configured

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,30 @@
+import { resolve } from 'path';
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+
+  env: {
+    BASE_URL: process.env.BASE_URL
+  },
+
   head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
     title: 'blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: []
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ['~assets/css/tailwind.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -32,10 +38,23 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+  modules: ['nuxt-purgecss'],
+
+  purgeCSS: {
+    mode: 'postcss',
+    enabled: process.env.NODE_ENV === 'production'
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    postcss: {
+      plugins: {
+        tailwindcss: resolve(__dirname, './tailwind.config.js')
+      }
+    }
+  },
+  tailwindcss: {
+    configPath: '~/tailwind.config.js',
+    cssPath: '~/assets/css/tailwind.css'
   }
-}
+};
